f31895f4ccc5d538eb08c223d2d0b820e4806d10
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-04-30 18:26:56 -0500

    commit tree 8d15f90e01589c7d6b36e42ab730f90afba66537

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -13,5 +17,9 @@ class Post < ActiveRecord::Base
 
   def make_title_case
     self.title = self.title.titlecase
+  end
+
+  def email_author_about_post
+    
   end
 end
